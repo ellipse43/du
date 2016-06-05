6bd966a6c30b20c4a655a61e4abda52039ae70a2
ellipse42 <ellipse43@gmail.com>
2016-06-05 16:04:01 +0800

    update env

--- js/index.js
+++ js/index.js
@@ -8,9 +8,15 @@ import {
 } from 'react-native';
 
 import AV from 'avoscloud-sdk';
+import qiniu from 'react-native-qiniu';
 import HomeView from './components/homeView.js';
+import {envs} from './env.js';
 
-AV.initialize('barRELKtqSJTsSmQp4i4qQGq-gzGzoHsz', 'zfaGe33WatcyWKwnfzvxyyhx');
+console.log(process.env.NODE_ENV);
+const envMap = envs[process.env.NODE_ENV];
+AV.initialize(envMap.leancloud.AV_APP_ID, envMap.leancloud.AV_SECRET);
+qiniu.conf.ACCESS_KEY = envMap.qiniu.ACCESS_KEY;
+qiniu.conf.SECRET_KEY = envMap.qiniu.SECRET_KEY;
 
 class du extends Component {
   render() {
